3367993e268cbff2be3c4192e8da0b3ac156bdd6
Jenny Jones <jennyjonesigwe@yahoo.com>
2019-01-31 15:43:17 +0100

    [get/parties]: added get request to get all politicl parties

---
+++ db/parties.js
@@ -0,0 +1,16 @@
+let parties = [
+    {
+        id: 1,
+        party: 'PDP',
+    },
+    {
+        id: 2,
+        party: 'APC',
+    },
+    {
+        id: 3,
+        party: 'APGA',
+    },
+];
+
+module.exports = parties;
