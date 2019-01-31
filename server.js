c33aa361cac91c31347a9e0b76eae171340358dc
Jenny Jones <jennyjonesigwe@yahoo.com>
2019-01-31 23:48:06 +0100

    added get request for returning all political offices and a single office by its id

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const parties = require('./db/parties');
+const offices = require('./db/offices');
 
 const port = process.env.PORT || 3000;
 
@@ -33,6 +34,33 @@ app.get(`${apiVersion}parties/:id`, (req, res) => {
     }
     return res.status(400).send({
         message: 'party not found',
+    });
+});
+
+// get political offices
+app.get(`${apiVersion}offices`, (req, res) => {
+    if (offices.length > 0)
+    return res.status(200).send({
+        status: 200,
+        offices,
+        message: 'All the available offices',
+    })
+    return res.status(400).send({
+        message: 'Database is empty!',
+    })
+});
+
+// get a single office
+app.get(`${apiVersion}offices/:id`, (req, res) => {
+    const findOffice = offices.filter(office => office.id === parseInt(req.params.id, 10));
+    if (findOffice) {
+        return res.status(200).send({
+            office: findOffice,
+            message: 'gotten a single office',
+        });
+    }
+    return res.status(400).send({
+        message: 'office not found',
     });
 });
 
